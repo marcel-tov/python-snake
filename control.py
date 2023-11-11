c98fc7c6219c5515916d4cfdce99207df522e350
Marcel <anchors_hooves_0n@icloud.com>
2023-11-11 23:09:29 +0100

    add keyboard bindings to control

--- control.py
+++ control.py
@@ -1,20 +1,29 @@
+import turtle
+
 class Control:
     def __init__(self, head):
         self.head = head
 
-    def go_up(self):
+    def addKeyboardBindings(self, wn: turtle):
+        wn.listen()
+        wn.onkeypress(self.goUp, "w")
+        wn.onkeypress(self.goDown, "s")
+        wn.onkeypress(self.goLeft, "a")
+        wn.onkeypress(self.goRight, "d")
+
+    def goUp(self):
         if self.head.direction != "down":
             self.head.direction = "up"
 
-    def go_down(self):
+    def goDown(self):
         if self.head.direction != "up":
             self.head.direction = "down"
 
-    def go_left(self):
+    def goLeft(self):
         if self.head.direction != "right":
             self.head.direction = "left"
 
-    def go_right(self):
+    def goRight(self):
         if self.head.direction != "left":
             self.head.direction = "right"
 
