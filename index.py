35a8d9800e36607d0bc6b765c91e7e9c33031fc5
Marcel <anchors_hooves_0n@icloud.com>
2023-11-11 22:56:15 +0100

    Update index.py
    Import control; add movement key events

--- index.py
+++ index.py
@@ -1,13 +1,26 @@
 import time
 from screen import Screen
+from snake import Snake
+from control import Control
 
 delay = 0.1
-screen = Screen()
-wn = screen.createScreen()
+wn = Screen().createScreen()
+head = Snake().createHead()
+control = Control(head)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(control.go_up, "w")
+wn.onkeypress(control.go_down, "s")
+wn.onkeypress(control.go_left, "a")
+wn.onkeypress(control.go_right, "d")
 
 # Main game loop
 while True:
     wn.update()
+
+    control.move()
+
     time.sleep(delay)
 
 wn.mainloop()
