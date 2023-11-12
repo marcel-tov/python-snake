39fdb40b0aabe36906089d516da7c6eefc7f8538
Marcel <anchors_hooves_0n@icloud.com>
2023-11-12 08:08:21 +0100

    add food

--- index.py
+++ index.py
@@ -2,12 +2,15 @@ from time import sleep
 from screen import Screen
 from snake import Snake
 from control import Control
+from food import Food
 
 delay = 0.1
 wn = Screen().createScreen()
 head = Snake().createHead()
 control = Control(head)
 control.addKeyboardBindings(wn)
+food = Food()
+food.moveToRandom()
 
 # Main game loop
 while True:
