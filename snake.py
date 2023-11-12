f4da5f0c9411d0812623d7259ee56ec823aaecf2
Marcel <anchors_hooves_0n@icloud.com>
2023-11-12 08:08:53 +0100

    Change snake color to green

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ class Snake:
         head = turtle.Turtle()
         head.speed(0)
         head.shape("square")
-        head.color("black")
+        head.color("green")
         head.penup()
         head.goto(0, 0)
         head.direction = "stop"
